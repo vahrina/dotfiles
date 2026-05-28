eab1760aa3afe2fc2a6f25f0c34010480cb060e7
vah <148314747+vahrina@users.noreply.github.com>
2026-05-28 19:27:13 +0200

    add `hist.zsh` (invoke with ctrl-r)

---
+++ home/.config/zsh/functions/hist.zsh
@@ -0,0 +1,19 @@
+hist() {
+  local selected
+  selected=$(
+    fc -nrl 1 \
+      | fzf \
+        --height=45% \
+        --border=rounded \
+        --pointer='*' \
+        --query="$BUFFER" \
+        --bind='ctrl-r:toggle-sort' \
+        --preview-window='bottom:3:wrap:hidden'
+    )
+
+    ret=$?
+    [[ $ret -eq 0 ]] && BUFFER=$selected && CURSOR=$#BUFFER
+    zle reset-prompt
+}
+zle -N hist
+bindkey '^R' hist
